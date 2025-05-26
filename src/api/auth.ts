e4215aac5ae7ecbf8a88c1617692ede023944b9c
Bijin10 <john@hiregradus.com>
2025-05-26 13:26:02 +0930

    Fix API URL for production deployment

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://youthalive-backend-873403ae276a.herokuapp.com/api';
+const API_BASE_URL = 'https://youthalive-backend-873403ae276a.herokuapp.com/api';
 
 // Debug log to see what URL is being used
 console.log('Auth API_BASE_URL:', API_BASE_URL);
